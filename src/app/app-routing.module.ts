6806046097a93cee578a8b3f0ca83966fd086d54
Anderson Junior Rodrigues <andersonkz@ymail.com>
2018-03-14 01:32:11 -0300

    criando html produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,35 +6,35 @@ import { VendedorComponent } from './vendedor/vendedor.component';
 import { VendaComponent } from './venda/venda.component';
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        redirectTo: '/comissoes',
-        pathMatch: 'full'
-    },
-    {
-        path: 'comissoes',
-        component: ComissaoComponent
-    },
-    {
-        path: 'produto',
-        component: ProdutoComponent
-    },
-    {
-        path: 'vendedor',
-        component: VendedorComponent
-    },
-    {
-        path: 'venda',
-        component: VendaComponent
-    }
+  {
+    path: '',
+    redirectTo: '/comissoes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'comissoes',
+    component: ComissaoComponent
+  },
+  {
+    path: 'produto',
+    component: ProdutoComponent
+  },
+  {
+    path: 'vendedor',
+    component: VendedorComponent
+  },
+  {
+    path: 'venda',
+    component: VendaComponent
+  }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes)
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
