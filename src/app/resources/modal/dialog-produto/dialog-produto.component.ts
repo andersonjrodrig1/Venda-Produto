e529d22bbb18f26aebe510f9cd06e5942026a093
Anderson Junior Rodrigues <andersonkz@ymail.com>
2018-05-17 02:30:17 -0300

    criando modal

---
+++ src/app/resources/modal/dialog-produto/dialog-produto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-dialog-produto',
+  templateUrl: './dialog-produto.component.html',
+  styleUrls: ['./dialog-produto.component.css']
+})
+export class DialogProdutoComponent {
+
+}
